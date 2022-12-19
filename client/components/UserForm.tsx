6731a5850b4382829415f63429faac095655d866
Rasmus Elmgren <rasmus@elmgren.net>
2022-12-19 23:08:34 +0100

    form styling

--- client/components/UserForm.tsx
+++ client/components/UserForm.tsx
@@ -5,9 +5,10 @@ import InputField from './InputField'
 const UserForm = () => {
   return (
     <form>
-      <div className="flex flex-col justify-center align-middle">
+      <div className="flex flex-col items-center">
         <InputField labelText='Username:' type='text' name='username' placeholderText='username' required/>
         <InputField labelText='Password:' type='password' name='password' placeholderText='password' required/>
+        <br />
         <Button buttonText='Get Comments' type='button'/>
       </div>
     </form>
