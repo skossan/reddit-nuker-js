46c4202b32a64d83368b0313baa270f2d72fd91d
Rasmus Elmgren <rasmus@elmgren.net>
2022-12-20 11:23:06 +0100

    fixing state for the form

--- client/components/Button.tsx
+++ client/components/Button.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 interface Props {
   buttonText: string;
-  type: "button";
+  type?: "button" | "submit";
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const Button = ({buttonText, type, onClick}: Props) => {
   return (
-    <button type={type} onClick={onClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-80">{buttonText}</button>
+    <button type={type || "submit"} onClick={onClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-80">{buttonText}</button>
   )
 }
 
